feb1d8b99e4ee31d20a56f8a118500a104381a3f
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-03-16 08:40:24 -0700

    Add missing files from last commit
    Change-Id: I08c0b2dcac209f7b8ba79fc39a342fe2b7f37198

---
+++ src/runtime/mem_wasm.go
@@ -0,0 +1,28 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build wasm
+
+package runtime
+
+import "unsafe"
+
+// https://webassembly.github.io/spec/core/exec/runtime.html#memory-instances
+const _PAGESIZE = 64 * 1024
+
+func sbrk(n uintptr) unsafe.Pointer {
+	grow := (int32(n) + _PAGESIZE - 1) / _PAGESIZE
+	size := currentMemory()
+
+	if growMemory(grow) < 0 {
+		return nil
+	}
+
+	resetMemoryDataView()
+	return unsafe.Pointer(uintptr(size) * _PAGESIZE)
+}
+
+// Implemented in src/runtime/sys_wasm.s
+func currentMemory() int32
+func growMemory(pages int32) int32
