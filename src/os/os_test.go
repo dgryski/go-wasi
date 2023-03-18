27a0c4b41753c81d8f1e0ef05f5d643a17f72ca2
Achille Roussel <achille.roussel@gmail.com>
2023-03-17 22:19:53 -0700

    skip TestPipeThreads in os package since os.Pipe is not supported
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- src/os/os_test.go
+++ src/os/os_test.go
@@ -2572,6 +2572,8 @@ func TestPipeThreads(t *testing.T) {
 		t.Skip("skipping on Plan 9; does not support runtime poller")
 	case "js":
 		t.Skip("skipping on js; no support for os.Pipe")
+	case "wasip1":
+		t.Skip("skipping on wasip1; no support for os.Pipe")
 	}
 
 	threads := 100
