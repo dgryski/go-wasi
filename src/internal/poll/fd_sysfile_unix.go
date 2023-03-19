db9a9cdbc877eea506310f550d3523da858167a9
Achille Roussel <achille.roussel@gmail.com>
2023-03-19 14:10:23 -0700

    remove unnecessary constructs and declarations
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- src/internal/poll/fd_sysfile_unix.go
+++ src/internal/poll/fd_sysfile_unix.go
@@ -6,4 +6,22 @@
 
 package poll
 
-type SysFile struct{}
+import "syscall"
+
+type SysFile struct {
+	// Writev cache.
+	iovecs *[]syscall.Iovec
+}
+
+// dupCloseOnExecOld is the traditional way to dup an fd and
+// set its O_CLOEXEC bit, using two system calls.
+func dupCloseOnExecOld(fd int) (int, string, error) {
+	syscall.ForkLock.RLock()
+	defer syscall.ForkLock.RUnlock()
+	newfd, err := syscall.Dup(fd)
+	if err != nil {
+		return -1, "dup", err
+	}
+	syscall.CloseOnExec(newfd)
+	return newfd, "", nil
+}
